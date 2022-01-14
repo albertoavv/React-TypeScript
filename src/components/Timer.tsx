9d4b14b14f9505a31371688383f2889475ca636f
Alberto <albertoavv@hotmail.es>
2022-01-13 22:12:36 -0400

    react+typeScript

---
+++ src/components/Timer.tsx
@@ -0,0 +1,7 @@
+export const Timer = () => {
+    return (
+        <div>
+            <h4>Timer: <small>0</small></h4>            
+        </div>
+    )
+}
---
+++ src/components/Usuario.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+
+interface User  {
+    uid: string;
+    name: string;
+}
+
+export const Usuario = () => {
+
+    const [user, setUser] = useState<User>()
+
+    const login = () => {
+        setUser({
+            uid: 'ABC123', 
+            name: 'Alberto Valdes'
+        });
+    }
+    return (
+        <div className="mt-5">
+          <h3>Usuario: useState</h3>
+
+          <button onClick={ login }
+           className="btn btn-outline-primary">
+            login  
+          </button>
+
+          {
+              (!user) 
+              ?  <pre>No hay usuarios</pre>  
+                :
+                <pre>{ JSON.stringify( user ) }</pre>  
+          }
+
+        </div>
+    )
+}
