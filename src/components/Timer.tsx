947314921707ad1a86b2b554dd511e91c9e95067
Alberto <albertoavv@hotmail.es>
2022-01-15 21:09:33 -0400

    de un componente padre a uno hijo, y se utiliza en Timer.tsx utilizando useEffect, useRef in TypeScript

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,27 @@
-export const Timer = () => {
+import { useEffect, useRef, useState } from 'react';
+
+type TimerArgs = {
+    milisegundos: number, 
+    //segundos?: number,
+}
+
+export const Timer = ({ milisegundos }:  TimerArgs ) => {
+
+    const [segundos, setSegundos] = useState(0);
+    const ref = useRef<NodeJS.Timeout>();
+
+    console.log(milisegundos);
+    
+
+    useEffect(() => {
+        //console.log('useEffect');
+        ref.current && clearInterval( ref.current)
+        ref.current =  setInterval( () => setSegundos( s => s + 1 ) , milisegundos );
+        
+    }, [milisegundos])
     return (
-        <div>
-            <h4>Timer: <small>0</small></h4>            
-        </div>
+        <>
+            <h4>Timer: <small>{ segundos }</small></h4>            
+        </>
     )
 }
--- src/components/TimerPadre.tsx
+++ src/components/TimerPadre.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { Timer } from "./Timer"
 
 export const TimerPadre = () => {
-    return (
+  
+  const [milisegundos, setMilisegundos] = useState( 1000 )
+  
+  return (
         <>
           <span>Milisegundos { 1000 }</span>  
-          <Timer />
+
+          <br />
+
+          <button className = "btn btn-outline-success"          
+              onClick={ () => setMilisegundos(1000) }>
+              1000 
+          </button>
+          <button className = "btn btn-outline-success"
+              onClick={ () => setMilisegundos(2000) }>
+              2000 
+          </button>
+          <Timer milisegundos={ milisegundos }/>
         </>
     )
 }
