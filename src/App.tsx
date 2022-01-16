52063bb4fbc26f019f6d6e059a7969463786fe31
Alberto <albertoavv@hotmail.es>
2022-01-15 22:25:35 -0400

    React + TypeScript: useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ContadorRed } from "./components/ContadorRed";
 import { Counter } from "./components/Counter";
 import { TimerPadre } from "./components/TimerPadre";
 import { Usuario } from "./components/Usuario";
@@ -15,6 +16,10 @@ function App() {
       <hr />
 
       <TimerPadre />
+
+      <h2>useReducer</h2>
+      <hr />
+      <ContadorRed />
 
     </>
   );
